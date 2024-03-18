aa5445547e434df6c5305a47e4a1662b49786595
tiesen243 <ttien56906@gmail.com>
2024-03-18 21:27:38 +0700

    dasdsasad

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,8 @@
 import { app } from '@/server/elysia'
-import { edenTreaty } from '@elysiajs/eden'
+import { edenTreaty, treaty } from '@elysiajs/eden'
 import { baseUrl } from './site'
+
+// uncomment the line below will throw an error
+/* export const api = treaty(app).api.elysia */
 
 export const api = edenTreaty<typeof app>(baseUrl).api.elysia
