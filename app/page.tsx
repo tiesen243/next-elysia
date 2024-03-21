d76671ac5190245a5062c7243d917c0c33f3bb89
tiesen243 <ttien56906@gmail.com>
2024-03-22 03:41:53 +0700

    rp

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,26 @@ import { auth } from '@/server/auth'
 
 const Page: NextPage = async () => {
   const session = await auth()
+  const size = 120
   return (
     <>
       <div className="flex aspect-video flex-col items-center justify-center gap-20">
-        <div className="relative z-[-1] flex items-center gap-8 before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-          <Image src="/nextjs.svg" width={150} height={150} alt="Next.js logo" />
+        <div className="relative z-[-1] flex items-center gap-8 before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] md:gap-12 lg:gap-16 before:lg:h-[360px]">
+          <Image
+            src="/nextjs.svg"
+            width={size}
+            height={size}
+            className="size-28 md:size-40 lg:size-64"
+            alt="Next.js logo"
+          />
           <span className="text-4xl font-bold">X</span>
-          <Image src="/elysia.svg" width={150} height={150} alt="Elysiajs logo" />
+          <Image
+            src="/elysia.svg"
+            width={size}
+            height={size}
+            className="size-28 md:size-40 lg:size-64"
+            alt="Elysiajs logo"
+          />
         </div>
 
         <div className="flex gap-4">
@@ -33,8 +46,7 @@ const Page: NextPage = async () => {
         </div>
       </div>
 
-      {/* {!session || !session.user ? null : <CreateForm />} */}
-      <CreateForm />
+      {!session || !session.user ? null : <CreateForm />}
 
       <PostList userId={!session || !session.user ? '' : session.user.id} />
     </>
