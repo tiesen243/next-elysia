41edbee8e0e8a5dd93b3c817c0f27e4a33750824
tiesen243 <ttien56906@gmail.com>
2024-03-25 11:53:17 +0700

    das

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,14 @@ import CreateForm from '@/components/create-form'
 import PostList from '@/components/post-list'
 import { Button } from '@/components/ui/button'
 import { auth } from '@/server/auth'
+import { Typography } from '@/components/ui/typography'
 
 const Page: NextPage = async () => {
   const session = await auth()
   const size = 120
   return (
     <>
+      <Typography>{process.env.VERCEL_URL}</Typography>
       <div className="flex aspect-video flex-col items-center justify-center gap-20">
         <div className="relative z-[-1] flex items-center gap-8 before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] md:gap-12 lg:gap-16 before:lg:h-[360px]">
           <Image
