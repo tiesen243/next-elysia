1f8826eacd50e04c2eead7ec4d051217e76a5790
tiesen243 <ttien56906@gmail.com>
2024-03-25 12:07:25 +0700

    dsad

--- lib/site.ts
+++ lib/site.ts
@@ -1,6 +1,6 @@
 export const getBaseUrl = () => {
   if (typeof window !== 'undefined') return window.location.origin
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
+  if (process.env.APP_URL) return `https://${process.env.APP_URL}`
   return `http://localhost:${process.env.PORT ?? 3000}`
 }
 
